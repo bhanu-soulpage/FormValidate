60ac6f5aab3812e2192ce8c5f10b609677cd6f88
bhanuprakash8989 <bhanuprakash.ravipati@soulpageit.com>
2022-11-01 19:03:58 +0530

    init

--- pages/signup.js
+++ pages/signup.js
@@ -26,7 +26,7 @@ import Link from 'next/link';
 
         console.log(enteredData)
         if (enteredData.password !== data.rpassword) {
-           setUserInfo()
+           setUserInfo(<p className="text-danger">You Entered Wrong Credentials</p>)
            setErrors(<p className="text-danger">{`Password did'nt match`}</p>) 
         } else {
             // setErrors('')
